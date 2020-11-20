892cded6d67db13fcd031a22cbee2cd2dc1dbdca
Adrian Pop <adi.pop.bv@gmail.com>
2020-11-20 17:04:04 +0200

    Added custom light and dark themes

--- front-end/lib/config.dart
+++ front-end/lib/config.dart
@@ -2,6 +2,22 @@ library config.global;
 import 'package:flutter/material.dart';
 import 'package:moove/themes/theme_controller.dart';
 
-ThemeData lightTheme = ThemeData.light();
-ThemeData darkTheme = ThemeData.dark();
+Color primaryColor = Colors.greenAccent;
+
+ThemeData lightTheme = ThemeData(
+  accentColor: primaryColor,
+  primaryColor: primaryColor,
+  appBarTheme: AppBarTheme(
+    color: primaryColor
+  )
+);
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  accentColor: primaryColor
+);
 ThemeController themeController = ThemeController();
+<<<<<<< Updated upstream
+=======
+
+Radius defaultRoundedCorner = Radius.circular(40);
+>>>>>>> Stashed changes
