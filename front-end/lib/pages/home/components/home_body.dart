80ba8e17d21a3cf8e3f6dc5d8de668fcecb506cc
Alexandru-Mihai Suciu <alexandru.suciu@student.manchester.ac.uk>
2020-11-20 18:15:00 +0200

    Add progress bar for challanges on homescreen
    The progress value is hardcoded to be 50 for now and it should have a text displaying Challenge progress

--- front-end/lib/pages/home/components/home_body.dart
+++ front-end/lib/pages/home/components/home_body.dart
@@ -7,17 +7,20 @@ class HomeBody extends StatelessWidget {
     return Column(
       children: [
         Container(
-        height: screenSize.height * 0.3,
-          decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(35),
-                  bottomRight: Radius.circular(35)
-              )
-          ),
-        )
+            height: screenSize.height * 0.3,
+            decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(35),
+                    bottomRight: Radius.circular(35))),
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.grey,
+              value: 50,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+              semanticsLabel: "Challenge progress",
+              semanticsValue: "Semantics Value",
+            ))
       ],
     );
   }
-
 }
