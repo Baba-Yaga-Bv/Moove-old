f0f40bfc5f2b17f8249e2f46f390ae74f58643db
Adrian Pop <adi.pop.bv@gmail.com>
2020-11-21 01:09:41 +0200

    Added profile page base

---
+++ front-end/lib/pages/profile/profile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ProfilePageState();
+}
+
+class ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildProfileBar(),
+    );
+  }
+
+  AppBar buildProfileBar() {
+    return AppBar(
+      elevation: 0,
+
+    );
+  }
+}
