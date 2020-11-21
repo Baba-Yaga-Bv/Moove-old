fcd3a7a1cd66f6b361ae16670b5825b200e40113
Adrian Pop <adi.pop.bv@gmail.com>
2020-11-21 02:23:11 +0200

    Tweaked return home arrow and added top container

--- front-end/lib/pages/communities/communities.dart
+++ front-end/lib/pages/communities/communities.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moove/pages/home/home.dart';
 
+import '../../config.dart';
 import 'components/communities_body.dart';
 
 class CommunitiesPage extends StatefulWidget {
@@ -12,7 +13,24 @@ class CommunitiesPageState extends State<CommunitiesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildCommunitiesBar(),
       body: CommunitiesBody()
+    );
+  }
+
+  AppBar buildCommunitiesBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          iconSize: defaultIconSize,
+        )
+      ],
     );
   }
 
