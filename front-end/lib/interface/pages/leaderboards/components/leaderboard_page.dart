648814b057a0be44590063a24bbd91fed89ae1a2
Adrian Pop <adi.pop.bv@gmail.com>
2020-11-22 10:20:20 +0200

    Login now working

--- front-end/lib/interface/pages/leaderboards/components/leaderboard_page.dart
+++ front-end/lib/interface/pages/leaderboards/components/leaderboard_page.dart
@@ -1,62 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moove/domain/entities/user.dart';
 import 'package:moove/interface/config.dart';
 import 'package:moove/interface/pages/communities/components/CommunityListElement.dart';
 import 'package:moove/service/service.dart';
+import 'package:moove/main.dart';
 
-<<<<<<< Updated upstream
-
-class LeaderboardPage extends StatefulWidget{
-
-=======
 class LeaderboardPage extends StatefulWidget {
   CommunityListElement com;
   LeaderboardPage(this.com);
->>>>>>> Stashed changes
   @override
   State<StatefulWidget> createState() => LeaderboardPageState(com);
 }
 
-class LeaderboardPageState extends State<LeaderboardPage>
-{
+class LeaderboardPageState extends State<LeaderboardPage> {
   CommunityListElement com;
   LeaderboardPageState(this.com);
 
-
   @override
   Widget build(BuildContext context) {
-<<<<<<< Updated upstream
-    Leaderboard leaderboard=service.getLeaderboard(com.id);
-=======
     List<User> leaderboard = List<User>();
     service.getLeaderboard(com.id).then((List<User> value) {
       leaderboard = value;
       setState(() {});
     });
->>>>>>> Stashed changes
     return ListView.separated(
-      itemBuilder: (BuildContext context, int index)
-      {
-        return Row(
-          children: [
-            Text(
-              '${index}',
-              style: superSmallTextStyle,
-            ),
-            Text(
-              '${leaderboard[index].name}',
-              style: superSmallTextStyle,
-            ),
-            Text(
-              '${leaderboard[index].score}'
-            )
-          ],
-        );
-      },
-
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          return Row(
+            children: [
+              Text(
+                '${index}',
+                style: superSmallTextStyle,
+              ),
+              Text(
+                '${leaderboard[index].firstName + " " + leaderboard[index].lastName}',
+                style: superSmallTextStyle,
+              ),
+              Text('${leaderboard[index].points.value}')
+            ],
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
         itemCount: null);
-
-
   }
 }
